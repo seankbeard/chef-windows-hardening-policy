2c6304e0bd1b8cd60afd440c3a041461ebedfee2
Sean Beard <seanbeard@Seans-MacBook-Pro.local>
2019-12-19 13:30:28 +1300

    initial commit

---
+++ harden.rb
@@ -0,0 +1,16 @@
+# base.rb - Describe how you want Chef to build your system.
+#
+# For more information on the Policyfile feature, visit
+# https://github.com/opscode/chef-dk/blob/master/POLICYFILE_README.md
+
+# A name that describes what the system you're building with Chef does.
+name 'harden'
+
+# Where to find external cookbooks:
+default_source :supermarket
+
+# Specify a custom source for a cookbook:
+cookbook 'windows-hardening', git: 'https://github.com/dev-sec/chef-windows-hardening'
+
+# run_list: chef-client will run these recipes in the order specified.
+run_list 'windows-hardening'
